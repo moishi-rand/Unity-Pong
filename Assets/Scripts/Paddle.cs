04c28e5db96da4019224483ef60b81d05f62c8c8
moishi-rand <randmeb@gmail.com>
2021-08-19 22:08:49 +0300

    Added Move Ability

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+ public enum PlayerPaddle
+{
+    Paddle1,
+    Paddle2
+}
+
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float moveSpeed = 10;
+    [SerializeField]
+    private float movePositiveRange = 22.5f;
+    [SerializeField]
+    private float moveNegativeRange = -5f;
+
+    [SerializeField]
+    private PlayerPaddle myPaddle;
+
+    private bool acceptsInput = true;
+    
 
     // Update is called once per frame
     void Update()
     {
+        if (!acceptsInput)
+        {
+            return;
+        }
+
+        float input = 0;
+
+        if (myPaddle == PlayerPaddle.Paddle1)
+        {
+            input = Input.GetAxis("Vertical");
+        }
         
+        if(myPaddle == PlayerPaddle.Paddle2)
+        {
+            input = Input.GetAxis("Vertical2");
+        }
+
+        Vector3 newPosition = transform.position;
+
+        newPosition.y += input * moveSpeed * Time.deltaTime;
+
+        newPosition.y = Mathf.Clamp(newPosition.y, moveNegativeRange, movePositiveRange);
+
+        transform.position = newPosition;
     }
 }
